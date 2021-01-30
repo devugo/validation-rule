9986e0c9e899575e7ec4ba12ed51e3e4e41bca5f
Ezenwankwo Ugochukwu <ugoezenwankwo@gmail.com>
2021-01-30 07:42:40 +0100

    Validator rule for JSON payload

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,17 +1,8 @@
 // VALIDATION HELPER FUNCTIONS
 
-// validation conditions
-exports.conditions = [
-    'eq',
-    'neq',
-    'gt',
-    'gte',
-    'contains'
-]
-
-
 // Validator function
 exports.validate = (body) => {
+
     //  VALIDATE DATA STRUCTURE
 
     // Validate required fields
@@ -25,7 +16,22 @@ exports.validate = (body) => {
     if(validateDataTypeFields.code !== 200){
         return validateDataTypeFields
     }
+
+    //  Validate payload validity
+    let validatePayloadValidity = payloadType(body);
+    if(validatePayloadValidity.code !== 200){
+        return validatePayloadValidity
+    }
 }
+
+// validation conditions
+const conditions = [
+    'eq',
+    'neq',
+    'gt',
+    'gte',
+    'contains'
+]
 
 /**
  * Check if the required fields are present i.e rule and data
@@ -54,6 +60,19 @@ const dataTypeFields = (body) => {
     }
     if(typeof(body.data) !== 'object' && typeof(body.data) !== 'string'){
         return dataStructureMessage(400, 'data should be an object or an array or a string');
+    }
+
+    return dataStructureMessage(200);
+}
+
+/**
+ * Validate invalid JSON payload
+ * 
+ * @param {Request body} body 
+ */
+const payloadType = (body) => {
+    if(!body.rule.field || !body.rule.condition || !body.rule.condition_value || conditions.indexOf(body.rule.condition) === -1 || body.rule.field.split('.').length > 3){
+        return dataStructureMessage(400, 'Invalid JSON payload passed.');
     }
 
     return dataStructureMessage(200);
